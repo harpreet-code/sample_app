afdd2004233d26e207b3a0066d5993d17ae27f1d
Harpreet Sandhu <harpreet.sandhu@akqa.com>
2011-10-20 18:22:49 -0700

    Exercises for Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Return the sample app logo
+  def logo
+    return image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
